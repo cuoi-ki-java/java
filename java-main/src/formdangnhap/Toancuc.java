e7259b4d0601af7eca5fba5f5019722849cf50ee
phanvson05 <johndoe@example.com>
2024-06-13 21:15:55 +0700

    xuli

---
+++ java-main/src/formdangnhap/Toancuc.java
@@ -0,0 +1,48 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formdangnhap;
+
+/**
+ *
+ * @author This PC
+ */
+public class Toancuc {
+    
+    public static String tenkh="";
+    public static String tuoi=" ";
+    public static String tenkh2="";
+
+    public static String getTenkh2() {
+        return tenkh2;
+    }
+
+    public static void setTenkh2(String tenkh2) {
+        Toancuc.tenkh2 = tenkh2;
+    }
+    
+    public Toancuc(){
+        
+    }
+
+    public static String getTenkh() {
+        return tenkh;
+    }
+
+    public static void setTenkh(String tenkh) {
+        Toancuc.tenkh = tenkh;
+    }
+
+    public static String getTuoi() {
+        return tuoi;
+    }
+
+    public static void setTuoi(String tuoi) {
+        Toancuc.tuoi = tuoi;
+    }
+    
+    
+    
+}
